809a918064a04c6575f7c6b30b6e542914ddc6c5
SlimBe <102964430+SlimBe@users.noreply.github.com>
2022-04-08 16:34:30 +0100

    Add files via upload

---
+++ Day 4/Coque.java
@@ -0,0 +1,17 @@
+public class Coque{ 
+public int volume;
+private String couleur;
+final int IDENT2=2;
+
+Coque(){
+
+}
+Coque(int volume,String couleur, final int IDENT2){
+this.volume=volume;
+this.couleur=couleur;
+
+}
+boolean test(Boolean test){ 
+return(true);
+}
+}
